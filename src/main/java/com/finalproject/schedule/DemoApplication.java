cbbf95ddbb0ab8b0b4cdd7d64ece853c6c724934
hossein7979 <hosseinfouladi20@gmail.com>
2021-06-17 18:29:47 +0430

    added Swagger config

--- src/main/java/com/finalproject/schedule/DemoApplication.java
+++ src/main/java/com/finalproject/schedule/DemoApplication.java
@@ -2,8 +2,10 @@ package com.finalproject.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
 public class DemoApplication {
 	
 	public static void main(String[] args) {
@@ -11,3 +13,12 @@ public class DemoApplication {
 	}
 
 }
+/*spring.datasource.url=jdbc:mysql://localhost:3306/schedule
+spring.datasource.username=root
+spring.datasource.password=BEHIWZAD
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+spring.jpa.database-platform = org.hibernate.dialect.MySQL5InnoDBDialect
+spring.thymeleaf.cache=false
+spring.jpa.generate-ddl=true
+spring.jpa.hibernate.ddl-auto=update
+server.port=8055*/
