e6e4e726d9367b6a074a408a2e8acaaf81332693
hossein7979 <hosseinfouladi20@gmail.com>
2021-06-26 17:50:06 +0430

    added count of Masters

--- src/main/java/com/finalproject/schedule/MainController.java
+++ src/main/java/com/finalproject/schedule/MainController.java
@@ -1,11 +1,16 @@
 package com.finalproject.schedule;
 
+import com.finalproject.schedule.Modules.User.model.User;
+import com.finalproject.schedule.enums.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.finalproject.schedule.Modules.User.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -19,7 +24,14 @@ public class MainController {
 
     @RequestMapping(value = "/admin_main")
     public String admin_main(Model model){
-        model.addAttribute("main_model", userService.findAllUsers());
+     //   model.addAttribute("main_model", userService.findAllUsers());
+        List<User> userList=new ArrayList<>();
+        List<User> temp=userService.findAllUsers();
+        for(User user:temp){
+            if(user.getRoles().get(0).equals(Roles.MASTER))
+            userList.add(user);
+        }
+        model.addAttribute("master_length",userList.size());
         return "admin/admin_main";
     }
 
--- src/main/java/com/finalproject/schedule/Modules/User/controller/UserController.java
+++ src/main/java/com/finalproject/schedule/Modules/User/controller/UserController.java
@@ -2,6 +2,7 @@ package com.finalproject.schedule.Modules.User.controller;
 
 import com.finalproject.schedule.Modules.Announcements.model.Announce;
 import com.finalproject.schedule.Modules.Announcements.service.AnnounceService;
+import com.finalproject.schedule.enums.Roles;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.finalproject.schedule.Modules.User.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import com.finalproject.schedule.Modules.User.model.User;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -38,6 +40,20 @@ public class UserController {
         return "redirect:/Users";
     }
 
+    @RequestMapping(value = "/Master/numbers",method = RequestMethod.GET)
+    public String getAllMasters(Model model){
+        List<User>userList=new ArrayList<>();
+        List<User> temp=userService.findAllUsers();
+        for(User user:temp){
+
+                userList.add(user);
+
+        }
+        model.addAttribute("master_length",userList.size());
+        System.out.println(userList.size());
+        return  "/admin_main";
+
+    }
     @RequestMapping(value = "/delete/{codemelli}", method = RequestMethod.GET)
     public String delete(@PathVariable("codemelli") String codemelli) {
         userService.deleteByCodemelli(codemelli);
